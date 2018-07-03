b421029f30a9884465a38bae7bc7c282dee1a71c
DazzledAK <youwatchme22@gmail.com>
2018-07-03 11:28:30 -0500

    Added a deleteTodo method to delete todo's

--- src/App.js
+++ src/App.js
@@ -15,6 +15,11 @@ class App extends Component {
      };
    }
 
+   deleteTodo(todo) {
+     const toDoDelete = this.state.todos.filter(item => item !== todo);
+     this.setState({ todos: toDoDelete });
+   }
+
    handleChange(e) {
      this.setState({ newToDoDescription: e.target.value })
    }
@@ -38,7 +43,7 @@ class App extends Component {
       <div className="App">
         <ul>
         { this.state.todos.map( (todo, index) =>
-          <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
+          <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } deleteTodo={ () => this.deleteTodo(todo) } />
         )}
         </ul>
         <form onSubmit={ (e) => this.handleSubmit(e) }>
